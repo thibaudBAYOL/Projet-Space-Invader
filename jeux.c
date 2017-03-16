af62743d31bc409bbcc3c54a75f67adc5eebfdf1
Thibaud Bayol <thibaud.bayol@etu.u-pec.fr>
2017-03-16 20:16:56 +0100

    vaiseauxDebut

--- jeux.c
+++ jeux.c
@@ -1,6 +1,28 @@
 
-
+#include <stdoi.h>
+#include <stdlib.h>
 
 #include <string.h>
+
+
+#include <termios.h>
+       #include <unistd.h> // terminos
+
+#include <sys/types.h>
+       #include <sys/stat.h>
+       #include <fcntl.h> // open
+
+
+int main(int argc, const char* argv[]){
+
+char terre[24][81];
+
+
+afvaiseau( x, y,terre);
+
+
+
+return EXIT_SUCCESS;
+}
 
 
