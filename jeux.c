873d4f4d453b6cc037daff34108189d848886786
Thibaud Bayol <thibaud.bayol@etu.u-pec.fr>
2017-03-19 12:12:37 +0100

    Update jeux.c(type Vaisseaux)

--- jeux.c
+++ jeux.c
@@ -1,4 +1,3 @@
-
 #include <stdoi.h>
 #include <stdlib.h>
 
@@ -13,12 +12,40 @@
        #include <fcntl.h> // open
 
 
+struct Type{
+    int vit;// vitesse du tire en case / seconde
+    int cad;// cadence de tire ex: 0.5
+    char tir;// apparence du tire
+    int vit;// vie par defaut
+    
+    int h;// nb de case en hauteur
+    int l;// nb de case en largeur
+    char** visuel;// aparence du vaiseau
+}
+
+struct Vaisseau{
+    int x;//position x,y
+    int y;
+    
+    struct Type cat;// la categorie de vaiseau (cat: 1, 2, ...)
+    int vie;// le nb de vit reel du vaisseau
+}
+type def struct Vaisseau Vaisseau;
+
 int main(int argc, const char* argv[]){
 
-char terre[24][81];
 
 
-afvaiseau( x, y,terre);
+//lire les niveaux
+    int nb_vaiseau=0;
+    // lire les categories(type)
+    // enregistrer les vaisseaux (dans un tableau)
+    Vaiseau armer[nb_vaiseau]
+
+// afficher les vaiseaux;
+
+
+
 
 
 
